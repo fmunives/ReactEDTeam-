4e75947d2ef998e9684fbf3888393e99edc1cb25
Freddy Munive <munivef@yahoo.com>
2019-10-03 18:15:54 -0500

    Agrega provider a nuestra app

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
 import './styles/styles.scss';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import { getAllSpecialities } from './redux/actionCreators';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+store.dispatch(getAllSpecialities());
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
